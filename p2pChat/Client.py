d74eca518a266769ff11f33395b145437b0f2b7c
esraa turky <42881629+esraa-torky@users.noreply.github.com>
2022-01-08 16:19:23 +0300

    login with TCP and UDP connection created

--- p2pChat/Client.py
+++ p2pChat/Client.py
@@ -1,32 +1,53 @@
 import socket
 
 
-class Clinet:
+class Client:
     def __init__(self):
-        self.ip = '127.0.0.1'
-        self.port = 1234
-        # creating the TCP socket
-        self.serverTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.serverTCP.connect((self.ip, self.port))
+        self.ip = "127.0.0.1"
+        self.port = 20001
+        self.bufferSize = 1024
+        self.TCPConnection()
 
     def getinfo(self):
-        print(self.serverTCP.recv(1024).decode('utf-8'))
+        print(self.serverTCP.recv(self.bufferSize).decode('utf-8'))
         self.serverTCP.send(bytes(input(), 'utf-8'))
         while True:
             name = input('user name :')
             password = input('password :')
             self.serverTCP.send(bytes(name, 'utf-8'))
             self.serverTCP.send(bytes(password, 'utf-8'))
-            respond = self.serverTCP.recv(1024).decode('utf-8')
+            respond = self.serverTCP.recv(self.bufferSize).decode('utf-8')
             if respond == 'pass':
                 print(respond)
                 break
             else:
                 print(respond)
 
+    def TCPConnection(self):
+        # creating the TCP socket
+        self.serverTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serverTCP.connect((self.ip, 1234))
+        self.getinfo()
+        self.UDPConnection()
+
+    def UDPConnection(self):
+        self.serverUDP = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        msgFromClient = "Hello UDP Server"
+        bytesToSend = str.encode(msgFromClient)
+        self.serverUDP.sendto(bytesToSend, (self.ip, self.port))
+        msgFromServer = self.serverUDP.recvfrom(self.bufferSize)
+        msg = "Message from Server {}".format(msgFromServer[0])
+        print(msg)
+
+    def connectToPeer(self):
+        while True:
+            print('waiting for other Users..')
+            msgFromServer = self.serverUDP.recvfrom(self.bufferSize)
+            print(msgFromServer)
+
+
 def main():
-    clinet = Clinet()
-    clinet.getinfo()
+    client = Client()
 
 
 main()
--- p2pChat/Registry.py
+++ p2pChat/Registry.py
@@ -1,35 +1,36 @@
 import socket
-import threading
 
 
-class Registry:
+class Server:
     def __init__(self):
-        self.ip = '127.0.0.1'
-        self.port = 1234
+        self.ipUDP = "127.0.0.1"
+        self.portUDP = 20001
+        self.bufferSize = 1024
+        self.clients = []
+        self.onlineClients = []
+        self.serverUDP = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.serverUDP.bind((self.ipUDP, self.portUDP))
         # creating the TCP socket
         self.serverTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.serverTCP.bind((self.ip, self.port))
-        self.clients = []
-        self.ThreadCount = 0
-        self.onlineClients = []
+        self.serverTCP.bind((self.ipUDP, 1234))
+        self.TCPConnection()
 
     def readUsers(self):
         file = open('users.txt', 'r')
         for i in file.readlines():
-            self.clients.append({'name':i.split(' ')[0],'password':i.split()[1]})
+            self.clients.append({'name': i.split(' ')[0], 'password': i.split()[1]})
         file.close()
 
-    def addUserToFile(self,name,password):
-        file=open('users.txt','a')
+    def addUserToFile(self, name, password):
+        file = open('users.txt', 'a')
         file.write('\n')
-        file.write(name+' ')
+        file.write(name + ' ')
         file.write(password)
         file.close()
 
     def login_threaded(self, connection, address):
         self.readUsers()
         connection.send(bytes('Welcome to the Server write NEW for new account or OLD if you have an account', 'utf-8'))
-        print('Welcome to the Server')
         userType = connection.recv(1024).decode('utf-8')
         found = False
         if userType == 'NEW':
@@ -64,19 +65,30 @@ class Registry:
                 else:
                     connection.send(bytes('user name or password is wrong !!', 'utf-8'))
         connection.close()
+        self.UDPConnection()
 
-    # client login via TCP connection
-    def clientLogin(self):
+    def UDPConnection(self):
+        msgFromServer = "Hello UDP Client"
+        bytesToSend = str.encode(msgFromServer)
+        bytesAddressPair = self.serverUDP.recvfrom(self.bufferSize)
+        message = bytesAddressPair[0]
+        address = bytesAddressPair[1]
+        clientMsg = "Message from Client:{}".format(message)
+        clientIP = "Client IP Address:{}".format(address)
+        print(clientMsg)
+        print(clientIP)
+        self.serverUDP.sendto(bytesToSend, address)
+
+    def TCPConnection(self):
         self.serverTCP.listen(5)
         while True:
             client, address = self.serverTCP.accept()
+            print('accepted')
             self.login_threaded(client, address)
 
 
-
 def main():
-    server = Registry()
-    server.clientLogin()
+    server = Server()
 
 
 main()
