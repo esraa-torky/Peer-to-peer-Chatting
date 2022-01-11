c69d7a30d773531340bb797aef6fddfab3eae2c4
esraa turky <42881629+esraa-torky@users.noreply.github.com>
2022-01-11 21:46:23 +0300

    GUI created / login / search / send messages / save the messages

--- p2pChat/Client.py
+++ p2pChat/Client.py
@@ -3,6 +3,7 @@ from threading import Thread
 from tkinter import *
 import socket
 import json
+
 import time
 
 
@@ -12,6 +13,16 @@ class Client(Thread):
         self.ip = "127.0.0.1"
         self.port = 20001
         self.bufferSize = 1024
+        self.serverUDP = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    def createMessagesfile(self,name):
+        file = open(name+'.txt', 'a')
+        return file
+
+    def saveMessages(self,name,message):
+        file=open(name + '.txt', 'a')
+        file.write(message +'\n')
+        file.close()
 
     def TCPConnection(self):
         # creating the TCP socket
@@ -39,18 +50,63 @@ class Client(Thread):
 
     def search(self,name):
         self.serverTCP.send(bytes(name, 'utf-8'))
-        print('waiting')
-    def getSearchResult(self):
-        msgFromServer = self.serverTCP.recv(self.bufferSize).decode('utf-8')
-        print(msgFromServer)
+        msgFromServer = self.serverTCP.recv(self.bufferSize)
+        msgFromServer=json.loads(msgFromServer.decode())
         return msgFromServer
+
     def wait(self):
-        msgFromServer = self.serverTCP.recv(self.bufferSize).decode('utf-8')
-        print(msgFromServer)
+        msgFromServer = self.serverTCP.recv(self.bufferSize)
+        msgFromServer = json.loads(msgFromServer.decode())
         return msgFromServer
-    def answerRequist(self):
-        self.serverTCP.send(bytes('OK', 'utf-8'))
-        # return 'OK'
+
+    def sendHalloToUDP(self):
+        msgFromClient = "Hello UDP Server"
+        bytesToSend = str.encode(msgFromClient)
+        self.serverUDP.sendto(bytesToSend, (self.ip, self.port))
+
+    def createChatConnection(self,address):
+        clientTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientTCP.bind((address[0], address[1]))
+        clientTCP.listen(1)
+        self.client, address = clientTCP.accept()
+        # client.send(bytes('welcome', 'utf-8'))
+
+    def connectToChat(self,address):
+        self.reciverTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.reciverTCP.connect((address[0], address[1]))
+
+    def chatR(self,name1,name2):
+        while True:
+            m=name1+': '+self.reciverTCP.recv(self.bufferSize).decode()
+            if m== 'LOGOUT':
+                print(m)
+                break
+            else:
+                print(m)
+            mesge=input(name2+': ')
+            if mesge == 'LOGOUT':
+                self.reciverTCP.send(bytes(mesge,'utf-8'))
+                break
+            else:
+                self.reciverTCP.send(bytes(mesge, 'utf-8'))
+
+    def chatS(self,name1,name2):
+        while True:
+            mesge = input(name1 + ': ')
+            self.saveMessages(name1+'_'+name2, name1+': '+mesge)
+            if mesge == 'LOGOUT':
+                self.client.send(bytes(mesge, 'utf-8'))
+                break
+            else:
+                self.client.send(bytes(mesge, 'utf-8'))
+            m = name2+': '+self.client.recv(self.bufferSize).decode()
+            self.saveMessages(name1+'_'+name2, m)
+            if m=='LOGOUT':
+                print(m)
+                break
+            else:
+                print(m)
+
 
 class GUI(Frame):
     def __init__(self, parent):
@@ -119,6 +175,8 @@ class GUI(Frame):
         Label(waitOrSearch_screen,text="").pack()
         # create a register button
         Button(waitOrSearch_screen,text="waite", height="2", width="30",command=lambda:[self.wait(),waitOrSearch_screen.destroy()]).pack()
+        Button(waitOrSearch_screen, text="send hallo to UDP ", height="2", width="30",
+               command=self.client.sendHalloToUDP).pack()
 
     def waiting(self):
         self.waiting_screen = Toplevel(self.parent)
@@ -127,7 +185,8 @@ class GUI(Frame):
         Label(self.waiting_screen, text="waiting for other users to join the app").pack()
         Button(self.waiting_screen, text="check", height="2", width="30",
                command=self.waitForUsers).pack()
-
+        Button(self.waiting_screen, text="send hallo to UDP ", height="2", width="30",
+               command=self.client.sendHalloToUDP).pack()
 
     def wait(self):
         rcv = threading.Thread(target=self.acceptRequist)
@@ -140,117 +199,208 @@ class GUI(Frame):
         Label(self.wait_screen, text="").pack()
         Label(self.wait_screen, text="or you can go and search").pack()
         Button(self.wait_screen, text="search", width=10, height=1,command=lambda: [self.waitOrSearch(), self.wait_screen.destroy()]).pack()
-
+        Button(self.wait_screen, text="send hallo to UDP ", height="2", width="30",
+               command=self.client.sendHalloToUDP).pack()
 
     def search(self):
         self.client.searchOrWait('OK')
-        search_screen = Toplevel(self.parent)
-        search_screen.title("search")
-        search_screen.geometry("300x250")
-        Label(search_screen, text="Please enter details below to find user").pack()
-        Label(search_screen, text="").pack()
+        self.search_screen = Toplevel(self.parent)
+        self.search_screen.title("search")
+        self.search_screen.geometry("300x250")
+        Label(self.search_screen, text="Please enter details below to find user").pack()
+        Label(self.search_screen, text="").pack()
         name = StringVar()
-        Label(search_screen, text="Username * ").pack()
-        username_login_entry = Entry(search_screen, textvariable=name)
+        Label(self.search_screen, text="Username * ").pack()
+        username_login_entry = Entry(self.search_screen, textvariable=name)
         username_login_entry.pack()
-        Label(search_screen, text="").pack()
-        Button(search_screen, text="search", width=10, height=1, command=lambda: [self.searchRequist(name.get())]).pack()
+        Label(self.search_screen, text="").pack()
+        Button(self.search_screen, text="search", width=10, height=1, command=lambda: [self.searchRequist(name.get())]).pack()
+        Button(self.search_screen, text="send hallo to UDP ", height="2", width="30",
+               command=self.client.sendHalloToUDP).pack()
 
-    def chat(self):
-        #self.name = name
-        # to show chat window
-        self.Window =Toplevel(self.parent)
-        self.Window.title("CHATROOM")
-        self.Window.resizable(width=False,
-                              height=False)
-        self.Window.configure(width=470,
-                              height=550,
-                              bg="#17202A")
-        self.labelHead = Label(self.Window,
-                               bg="#17202A",
-                               fg="#EAECEE",
-                               text='name',
-                               font="Helvetica 13 bold",
-                               pady=5)
-
-        self.labelHead.place(relwidth=1)
-        self.line = Label(self.Window,
-                          width=450,
-                          bg="#ABB2B9")
-
-        self.line.place(relwidth=1,
-                        rely=0.07,
-                        relheight=0.012)
-
-        self.textCons = Text(self.Window,
-                             width=20,
-                             height=2,
-                             bg="#17202A",
-                             fg="#EAECEE",
-                             font="Helvetica 14",
-                             padx=5,
-                             pady=5)
-
-        self.textCons.place(relheight=0.745,
-                            relwidth=1,
-                            rely=0.08)
-
-        self.labelBottom = Label(self.Window,
-                                 bg="#ABB2B9",
-                                 height=80)
-
-        self.labelBottom.place(relwidth=1,
-                               rely=0.825)
-
-        self.entryMsg = Entry(self.labelBottom,
-                              bg="#2C3E50",
-                              fg="#EAECEE",
-                              font="Helvetica 13")
-
-        # place the given widget
-        # into the gui window
-        self.entryMsg.place(relwidth=0.74,
-                            relheight=0.06,
-                            rely=0.008,
-                            relx=0.011)
-
-        self.entryMsg.focus()
-
-        # create a Send Button
-        self.buttonMsg = Button(self.labelBottom,
-                                text="Send",
-                                font="Helvetica 10 bold",
-                                width=20,
-                                bg="#ABB2B9",
-                                command=lambda: self.sendButton(self.entryMsg.get()))
-
-        self.buttonMsg.place(relx=0.77,
-                             rely=0.008,
-                             relheight=0.06,
-                             relwidth=0.22)
-
-        self.textCons.config(cursor="arrow")
-
-        # create a scroll bar
-        scrollbar = Scrollbar(self.textCons)
-
-        # place the scroll bar
-        # into the gui window
-        scrollbar.place(relheight=1,
-                        relx=0.974)
-
-        scrollbar.config(command=self.textCons.yview)
-
-        self.textCons.config(state=DISABLED)
-
-        # function to basically start the thread for sending messages
-
+    # def chatReciver(self,name):
+    #     rcv = threading.Thread(target=self.receiveR())
+    #     rcv.start()
+    #     self.Window = Toplevel(self.parent)
+    #     self.Window.title("CHAT ROOM")
+    #     self.Window.resizable(width=False,
+    #                           height=False)
+    #     self.Window.configure(width=470,
+    #                           height=550,
+    #                           bg='gray91')
+    #     self.labelHead = Label(self.Window,
+    #                            bg='#856ff8',
+    #                            fg='gray91',
+    #                            text=name,
+    #                            font="Helvetica 13 bold",
+    #                            pady=5)
+    #
+    #     self.labelHead.place(relwidth=1)
+    #     self.line = Label(self.Window,
+    #                       width=450,
+    #                       bg='#856ff8')
+    #
+    #     self.line.place(relwidth=1,
+    #                     rely=0.07,
+    #                     relheight=0.012)
+    #
+    #     self.textCons = Text(self.Window,
+    #                          width=20,
+    #                          height=2,
+    #                          bg='gray91',
+    #                          fg="black",
+    #                          font="Helvetica 14",
+    #                          padx=5,
+    #                          pady=5)
+    #
+    #     self.textCons.place(relheight=0.745,
+    #                         relwidth=1,
+    #                         rely=0.08)
+    #
+    #     self.labelBottom = Label(self.Window,
+    #                              bg='#856ff8',
+    #                              height=80)
+    #
+    #     self.labelBottom.place(relwidth=1,
+    #                            rely=0.825)
+    #
+    #     self.entryMsg = Entry(self.labelBottom,
+    #                           bg='#856ff8',
+    #                           fg='gray91',
+    #                           font="Helvetica 13")
+    #
+    #     self.entryMsg.place(relwidth=0.74,
+    #                         relheight=0.06,
+    #                         rely=0.008,
+    #                         relx=0.011)
+    #
+    #     self.entryMsg.focus()
+    #
+    #     self.buttonMsg = Button(self.labelBottom,
+    #                             text="Send",
+    #                             font="Helvetica 10 bold",
+    #                             width=20,
+    #                             bg='#856ff8',
+    #                             command=lambda: self.sendButton(self.entryMsg.get()))
+    #
+    #     self.buttonMsg.place(relx=0.77,
+    #                          rely=0.008,
+    #                          relheight=0.06,
+    #                          relwidth=0.22)
+    #
+    #     self.textCons.config(cursor="arrow")
+    #
+    #     # create a scroll bar
+    #     scrollbar = Scrollbar(self.textCons)
+    #
+    #     # place the scroll bar
+    #     # into the gui window
+    #     scrollbar.place(relheight=1,
+    #                     relx=0.974)
+    #
+    #     scrollbar.config(command=self.textCons.yview)
+    #
+    #     self.textCons.config(state=DISABLED)
+    #
+    # def chatClient(self,name):
+    #     # to show chat window
+    #     self.Window =Toplevel(self.parent)
+    #     self.Window.title("CHAT ROOM")
+    #     self.Window.resizable(width=False,
+    #                           height=False)
+    #     self.Window.configure(width=470,
+    #                           height=550,
+    #                           bg='gray91')
+    #     self.labelHead = Label(self.Window,
+    #                            bg='#856ff8',
+    #                            fg='gray91',
+    #                            text=name,
+    #                            font="Helvetica 13 bold",
+    #                            pady=5)
+    #
+    #     self.labelHead.place(relwidth=1)
+    #     self.line = Label(self.Window,
+    #                       width=450,
+    #                       bg='#856ff8')
+    #
+    #     self.line.place(relwidth=1,
+    #                     rely=0.07,
+    #                     relheight=0.012)
+    #
+    #     self.textCons = Text(self.Window,
+    #                          width=20,
+    #                          height=2,
+    #                          bg='gray91',
+    #                          fg="black",
+    #                          font="Helvetica 14",
+    #                          padx=5,
+    #                          pady=5)
+    #
+    #     self.textCons.place(relheight=0.745,
+    #                         relwidth=1,
+    #                         rely=0.08)
+    #
+    #     self.labelBottom = Label(self.Window,
+    #                              bg='#856ff8',
+    #                              height=80)
+    #
+    #     self.labelBottom.place(relwidth=1,
+    #                            rely=0.825)
+    #
+    #     self.entryMsg = Entry(self.labelBottom,
+    #                           bg='#856ff8',
+    #                           fg='gray91',
+    #                           font="Helvetica 13")
+    #
+    #     self.entryMsg.place(relwidth=0.74,
+    #                         relheight=0.06,
+    #                         rely=0.008,
+    #                         relx=0.011)
+    #
+    #     self.entryMsg.focus()
+    #
+    #     self.buttonMsg = Button(self.labelBottom,
+    #                                 text="Send",
+    #                                 font="Helvetica 10 bold",
+    #                                 width=20,
+    #                                 bg='#856ff8',
+    #                                 command=lambda: self.sendButton(self.entryMsg.get()))
+    #
+    #     self.buttonMsg.place(relx=0.77,
+    #                          rely=0.008,
+    #                          relheight=0.06,
+    #                          relwidth=0.22)
+    #
+    #     self.textCons.config(cursor="arrow")
+    #
+    #     # create a scroll bar
+    #     scrollbar = Scrollbar(self.textCons)
+    #
+    #     # place the scroll bar
+    #     # into the gui window
+    #     scrollbar.place(relheight=1,
+    #                     relx=0.974)
+    #
+    #     scrollbar.config(command=self.textCons.yview)
+    #
+    #     self.textCons.config(state=DISABLED)
+    #
+    #     # function to basically start the thread for sending messages
+    #
     # def sendButton(self, msg):
     #     self.textCons.config(state=DISABLED)
     #     self.msg = msg
     #     self.entryMsg.delete(0, END)
-    #     snd = threading.Thread(target=self.sendMessage)
-    #     snd.start()
+    #     self.textCons.config(state=NORMAL)
+    #     self.textCons.insert(END,
+    #                         self.username.get()+': '+ msg + "\n\n")
+    #
+    #     self.textCons.config(state=DISABLED)
+    #     self.textCons.see(END)
+    #     # snd = threading.Thread(target=self.sendMessage)
+    #     # snd.start()
+    #
+    # def receiveR(self):
 
     def sendLogInData(self,t):
         r=self.client.sendLogInData(self.username.get(),self.password.get())
@@ -269,28 +419,41 @@ class GUI(Frame):
                 self.password_entry.delete(0,END)
                 self.username_entry.delete(0,END)
                 Label(self.register_screen, text=r).pack()
+
     def waitForUsers(self):
         r = self.client.waitForUsers()
         print(r)
         if r == 'ENTER':
             self.waiting_screen.destroy()
             self.waitOrSearch()
+
     def searchRequist(self,name):
-        self.client.search(name)
-        rcv = threading.Thread(target=self.client.getSearchResult)
+        print(name)
+        rcv = threading.Thread(target=self.getSearchResult, args=(name,))
         rcv.start()
-        if rcv =='OK':
-            self.chat()
+
+
+    def getSearchResult(self,name):
+        msgFromServer = self.client.search(name)
+        if msgFromServer['address'] == 'BUSY':
+            Label(self.search_screen, text=msgFromServer).pack()
+        else:
+            #self.chat(msgFromServer['name'],'s')
+            self.client.createChatConnection(msgFromServer['address'])
+            self.client.chatS(self.username.get(),msgFromServer['name'])
+            self.search_screen.destroy()
+
     def acceptRequist(self):
         m=self.client.wait()
-        if m=='REQUEST':
-            Button(self.wait_screen, text="accept", width=10, height=1,
-                   command=lambda: [self.sendAnswer()]).pack()
-            #self.chat()
-    def sendAnswer(self):
-        self.client.answerRequist()
-        self.chat()
-        self.wait_screen.destroy()
+        print(m['address'])
+        if len(m)!=0:
+            #self.chat(m['name'],'c')
+            self.client.connectToChat(m['address'])
+            self.client.chatR(m['name'], self.username.get())
+            self.wait_screen.destroy()
+
+
+
 def main():
     root = Tk()
     root.geometry("300x250")
--- p2pChat/Registry.py
+++ p2pChat/Registry.py
@@ -1,8 +1,7 @@
 import socket
-import sys
 import _thread
 import json
-import time
+
 
 
 class Server:
@@ -17,6 +16,7 @@ class Server:
         # creating the TCP socket
         self.serverTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.serverTCP.bind((self.ipUDP, 1234))
+
 
     def readUsers(self):
         file = open('users.txt', 'r')
@@ -64,15 +64,15 @@ class Server:
                         found = True
                         break
                 if found:
-                    here = False
+                    here=False
                     for i in self.onlineClients:
                         if i['name'] == name:
                             connection.send(bytes('already online on another machine', 'utf-8'))
-                            here = True
+                            here =True
                             break
                     if not here:
-                        client = {'name': name, 'password': password, 'address': address,
-                                  'client': connection, 'state': 'HERE'}
+                        client={'name': name, 'password': password, 'address': address,
+                                                   'client': connection, 'state': 'HERE','UDPaddress':''}
                         self.onlineClients.append(client)
                         connection.send(bytes('pass', 'utf-8'))
                         print('DONE')
@@ -81,57 +81,69 @@ class Server:
                     connection.send(bytes('user name or password is wrong !!', 'utf-8'))
         self.checkConnection(client)
 
-    def UDPConnection(self, clientLocation):
-        msgFromServer = "Hello UDP Client " + self.onlineClients[clientLocation]['name']
-        bytesToSend = str.encode(msgFromServer)
+
+    def UDPConnection(self,client):
         bytesAddressPair = self.serverUDP.recvfrom(self.bufferSize)
         message = bytesAddressPair[0]
         address = bytesAddressPair[1]
-        self.onlineClients[clientLocation]['address'] = address
         clientMsg = "Message from Client:{}".format(message)
         clientIP = "Client IP Address:{}".format(address)
+        self.onlineClients[self.onlineClients.index(client)]['UDPaddress'] = address
         print(clientMsg)
         print(clientIP)
-        self.serverUDP.sendto(bytesToSend, address)
-        self.checkConnection(clientLocation)
+        print('first')
+        while True:
+            bytesAddressPair = self.serverUDP.recvfrom(self.bufferSize)
+            message = bytesAddressPair[0]
+            address = bytesAddressPair[1]
+            clientMsg = "Message from Client:{}".format(message)
+            clientIP = "Client IP Address:{}".format(address)
+            self.onlineClients[self.onlineClients.index(client)]['UDPaddress'] = address
+            print(clientMsg)
+            print(clientIP)
+
 
     def checkConnection(self, client):
         while True:
             print(client['client'].recv(1024).decode('utf-8'))
             if len(self.onlineClients) > 1:
                 client['client'].send(b'ENTER')
-                r = client['client'].recv(1024).decode('utf-8')
+                r=client['client'].recv(1024).decode('utf-8')
                 print(r)
                 if r != 'NO':
                     self.onlineClients[self.onlineClients.index(client)]['state'] = 'searching'
                     self.search(client)
                     break
                 else:
-                    self.onlineClients[self.onlineClients.index(client)]['state'] = 'waiting'
+                    self.onlineClients [self.onlineClients.index(client)]['state'] = 'waiting'
                     client['state'] = 'waiting'
             else:
                 client['client'].send(b'nothing')
 
-    def search(self, client):
+    def search(self,client):
         while True:
-            name = client['client'].recv(1024).decode('utf-8')
+            name=client['client'].recv(1024).decode('utf-8')
             print(name)
-            found = False
+            found=False
             for i in self.onlineClients:
                 if name == i['name']:
-                    found = True
-                    reciver = i
+                    found=True
+                    reciver=i
             if found and reciver['state'] == 'waiting':
-                reciver['client'].send(bytes('REQUEST', 'utf-8'))
-                answer = reciver['client'].recv(self.bufferSize)
-                print(answer)
-                client['client'].send(answer)
-            else:
-                client['client'].send(bytes('BUSY', 'utf-8'))
+                data1 = json.dumps({"address":client['address'] , "name": reciver['name']})
+                client['client'].send(data1.encode())
+                self.onlineClients[self.onlineClients.index(client)]['state'] = 'chatting'
+                data2 = json.dumps({"address": client['address'], "name": client['name']})
+                reciver['client'].send(data2.encode())
+                self.onlineClients[self.onlineClients.index(reciver)]['state'] = 'chatting'
 
-    def chat(self, client1, client2):
+            else:
+                data2 = json.dumps({"address":'BUSY'})
+                client['client'].send(data2.encode())
+
+    def chat(self,client1,client2):
         while True:
-            msg1 = client1.recv(self.bufferSize)
+            msg1=client1.recv(self.bufferSize)
             client2.send(msg1)
             msg2 = client2.recv(self.bufferSize)
             client1.send(msg2)
@@ -142,7 +154,7 @@ class Server:
         self.serverTCP.listen(5)
         client, address = self.serverTCP.accept()
         print('accepted')
-        _thread.start_new_thread(self.login_threaded, (client, address))
+        _thread.start_new_thread(self.login_threaded, (client,address))
         print(self.onlineClients)
 
 
@@ -150,6 +162,9 @@ def main():
     server = Server()
     while True:
         server.TCPConnection()
+
+
+
 
 
 main()
